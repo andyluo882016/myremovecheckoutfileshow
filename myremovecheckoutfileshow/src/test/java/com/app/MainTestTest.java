cb735d02fa426f2ec0e9b3797f0be24dec277f8e
andyluo882016 <andylu882016>
2022-02-26 11:42:44 -0800

    show v5 @2/26

--- myremovecheckoutfileshow/src/test/java/com/app/MainTestTest.java
+++ myremovecheckoutfileshow/src/test/java/com/app/MainTestTest.java
@@ -7,8 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTestTest {
     MainTest test = new MainTest();
     @Test
-    void add() {//
+    void add() {
         assertEquals(8, test.add(2, 6));
         assertNotNull(test.add(10,90));
+    }
+    @Test
+    public void multipleTest(){
+        assertEquals(16, test.multiple(2,8));
     }
 }
